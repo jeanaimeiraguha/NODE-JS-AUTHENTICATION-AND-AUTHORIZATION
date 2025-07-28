ec69583422575a7013942410527912658d295ce5
IRAGUHA Jean  Aime <jeanaimeiraguha@gmail.com>
2025-07-28 20:13:44 +0200

    Backend

--- server.js
+++ server.js
@@ -2,19 +2,29 @@ import mysql from 'mysql';
 import express from 'express';
 import cors from 'cors';
 
-const app = express();
+// const app = express();
 // app.use(express())
-app.use(express.json())
-// const PORT = process.env.PORT || 3000;
-// require("dotenv").config;
-import { configDotenv } from 'dotenv';
-// const db = mysql.createConnection({
+// app.use(express.json())
+// // const PORT = process.env.PORT || 3000;
+// // require("dotenv").config;
+// import { configDotenv } from 'dotenv';
+const db = mysql.createConnection({
+host:"localhost",
+user:"root",
+password:"",
+database:""
+})
+// const express = require('express');
+// const mysql = require('mysql');
+// const cors = require('cors');
+// const fs = require('fs');
+// const path = require('path');
+// const bcrypt = require('bcrypt');
 
-//     hos:process.env.DB_HOST,
-//     user:process.env.DB_USER,
-//     password:process.env.DB_PASSWORD,
-//     database:process.env.DB_NAME,
-// })
+const app = express();
+app.use(cors());
+app.use(express.json());
+
 db.connect((err)=>{
     if(err){
         console.error("Connection failed")
